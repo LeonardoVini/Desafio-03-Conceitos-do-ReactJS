3602994a8e3ba0be70b3d676203b6c568beced5b
Leonardo Oliveira <lleonardo@equinix.com>
2020-08-23 23:19:08 -0300

    Adicionadas as funções de adicionar e remover repositórios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(({ data: repositories }) => {
+      setRepositories(repositories)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const { data: repository } = await api.post('repositories', {
+      title: "Desafio Node.js",
+      url: "https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      techs: [
+        "NodeJS",
+        "ReactJS",
+        "Angular"
+      ]
+    })
+
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    console.log(id)
+
+    await api.delete(`repositories/${id}`)
+    setRepositories([...repositories.filter(repository => repository.id !== id)])
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
